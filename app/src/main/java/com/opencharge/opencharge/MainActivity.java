9708630e4427f59bb7ea11778cc8587b4f1589d4
Oriol Parcerisa Grau <Oriol Parcerisa Grau>
2017-03-17 15:05:09 +0100

    Canvis map

--- app/src/main/java/com/opencharge/opencharge/MainActivity.java
+++ app/src/main/java/com/opencharge/opencharge/MainActivity.java
@@ -1,10 +1,12 @@
 package com.opencharge.opencharge;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,22 @@ public class MainActivity extends AppCompatActivity {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message2");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String elvalor = (String)dataSnapshot.getValue();
+                //showMessage(elvalor);
+            }
 
-        myRef.setValue("John Maddennn!");
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        android.support.v4.app.FragmentTransaction FragmentTransaction = getSupportFragmentManager().beginTransaction();
+        MapsFragment mapsFragment = new MapsFragment();
+        FragmentTransaction.replace(R.id.map, mapsFragment).commit();
+
     }
 }
