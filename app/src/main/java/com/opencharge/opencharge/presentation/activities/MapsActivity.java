f8d75a8bc46ce2697f9b5c1660f9e6f1de08eb8c
Ferran Martin <ferranmatin@gmail.com>
2017-03-22 16:54:27 +0100

    create presentation package

--- app/src/main/java/com/opencharge/opencharge/presentation/activities/MapsActivity.java
+++ app/src/main/java/com/opencharge/opencharge/presentation/activities/MapsActivity.java
@@ -1,4 +1,4 @@
-package com.opencharge.opencharge;
+package com.opencharge.opencharge.presentation.activities;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.opencharge.opencharge.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
