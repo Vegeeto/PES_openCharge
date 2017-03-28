9c0909847f138aeeda15f5e4012d608b8a45bb18
Víctor Hervás Sánchez <Vctor Hervs Snchez>
2017-03-29 00:26:07 +0200

    Integracion del NavigationDrawer con fragments y google maps

---
+++ app/src/main/java/com/opencharge/opencharge/presentation/fragments/MapsFragment.java
@@ -0,0 +1,39 @@
+package com.opencharge.opencharge.presentation.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.opencharge.opencharge.R;
+
+/**
+ * Created by Victor on 28/03/2017.
+ */
+
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_maps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        MapFragment mapFragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
