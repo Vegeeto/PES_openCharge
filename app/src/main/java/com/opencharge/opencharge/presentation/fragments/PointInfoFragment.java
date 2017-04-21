0f4a04f4a82f5daa8f7d01cf0d9133dd1aed59cf
Ferran Martin <ferranmatin@gmail.com>
2017-04-21 15:39:56 +0200

    cleaning

--- app/src/main/java/com/opencharge/opencharge/presentation/fragments/PointInfoFragment.java
+++ app/src/main/java/com/opencharge/opencharge/presentation/fragments/PointInfoFragment.java
@@ -1,7 +1,5 @@
 package com.opencharge.opencharge.presentation.fragments;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.annotation.Nullable;
@@ -21,9 +19,6 @@ import com.opencharge.opencharge.presentation.locators.UseCasesLocator;
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PointInfoFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link PointInfoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
@@ -34,8 +29,6 @@ public class PointInfoFragment extends Fragment {
 
     private static final String ARG_POINT_ID = "point_id";
     private String pointId;
-
-    private OnFragmentInteractionListener mListener;
 
     public PointInfoFragment() {
         // Required empty public constructor
@@ -95,44 +88,5 @@ public class PointInfoFragment extends Fragment {
 
         getPointUseCase.setPointId(pointId);
         getPointUseCase.execute();
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
