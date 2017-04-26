640923611f7e8edab0d1715387460bb686695b13
Víctor Hervás Sánchez <Vctor Hervs Snchez>
2017-04-26 12:19:49 +0200

    improved search feature

--- app/src/main/java/com/opencharge/opencharge/presentation/activities/NavigationActivity.java
+++ app/src/main/java/com/opencharge/opencharge/presentation/activities/NavigationActivity.java
@@ -77,9 +77,9 @@ public class NavigationActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
