e771d04d44aa3862bec3e4c086d9970723749f59
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-04-26 11:42:39 +0200

    Point factory created

--- app/src/main/java/com/opencharge/opencharge/domain/Factories/PointFactory.java
+++ app/src/main/java/com/opencharge/opencharge/domain/Factories/PointFactory.java
@@ -1,8 +1,39 @@
 package com.opencharge.opencharge.domain.Factories;
 
+import com.opencharge.opencharge.domain.Entities.Point;
+
 /**
  * Created by Crjs on 19/04/2017.
  */
 
 public class PointFactory {
+
+    private static PointFactory instance;
+
+    private PointFactory() {}
+
+    public static PointFactory getInstance() {
+        if(instance == null) {
+            instance = new PointFactory();
+        }
+        return instance;
+    }
+
+    public Point createNewPoint(double lat, double lon, String town, String street, String number,
+                                String accesType, String connectorType, String schedule) {
+
+        Point p = new Point();
+        p.setLat(lat);
+        p.setLon(lon);
+        p.setTown(town);
+        p.setStreet(street);
+        p.setNumber(number);
+        p.setAccessType(accesType);
+        p.setConnectorType(connectorType);
+        p.setSchedule(schedule);
+        return p;
+    }
+    public void setPointId(Point p, String id){
+        p.id = id;
+    }
 }
