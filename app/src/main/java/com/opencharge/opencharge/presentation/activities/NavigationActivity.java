f13b5a74fc744ee492e274f4702d637d6daab71f
Xictrov <vimar_cam@hotmail.com>
2017-04-27 00:12:41 +0200

    navigation drawer modified

--- app/src/main/java/com/opencharge/opencharge/presentation/activities/NavigationActivity.java
+++ app/src/main/java/com/opencharge/opencharge/presentation/activities/NavigationActivity.java
@@ -91,14 +91,9 @@ public class NavigationActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_maps) {
             fm.beginTransaction().replace(R.id.content_frame, new MapsFragment()).commit();
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_newpoint) {
 
         } else if (id == R.id.nav_share) {
 
