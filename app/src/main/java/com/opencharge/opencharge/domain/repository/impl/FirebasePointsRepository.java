d71918db0f432667945c37a5d35b9e9c21737519
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-05-19 12:40:30 +0200

    changed the reference for PointTest (WARNING: this should be changed when the new DB is loaded)

--- app/src/main/java/com/opencharge/opencharge/domain/repository/impl/FirebasePointsRepository.java
+++ app/src/main/java/com/opencharge/opencharge/domain/repository/impl/FirebasePointsRepository.java
@@ -31,7 +31,7 @@ public class FirebasePointsRepository implements PointsRepository {
 
     @Override
     public void getPoints(final GetPointsCallback callback) {
-        DatabaseReference myRef = database.getReference("Points");
+        DatabaseReference myRef = database.getReference("PointsTest");
 
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -50,7 +50,7 @@ public class FirebasePointsRepository implements PointsRepository {
 
     @Override
     public void getPointById(String pointId, final GetPointByIdCallback callback) {
-        DatabaseReference myRef = database.getReference("Points").child(pointId);
+        DatabaseReference myRef = database.getReference("PointsTest").child(pointId);
 
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -92,12 +92,11 @@ public class FirebasePointsRepository implements PointsRepository {
 
     public void createPoint(FirebasePoint point, final CreatePointCallback callback) {
 
-        DatabaseReference myRef = database.getReference("Points");
+        DatabaseReference myRef = database.getReference("PointsTest");
         myRef.push().setValue(point, new DatabaseReference.CompletionListener() {
 
             @Override
             public void onComplete(DatabaseError de, DatabaseReference dr) {
-                System.out.println("Record saved!");
                 Log.d("CrearPunt","Record saved!");
                 String postId = dr.getKey();
                 callback.onPointCreated(postId);
