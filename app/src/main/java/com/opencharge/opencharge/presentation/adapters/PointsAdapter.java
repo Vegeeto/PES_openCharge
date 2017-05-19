ee846af293b41fa944073bde72c0aa929ffb17ec
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-05-19 12:39:23 +0200

    points adapter changes

--- app/src/main/java/com/opencharge/opencharge/presentation/adapters/PointsAdapter.java
+++ app/src/main/java/com/opencharge/opencharge/presentation/adapters/PointsAdapter.java
@@ -1,14 +1,19 @@
 package com.opencharge.opencharge.presentation.adapters;
 
 import android.content.Context;
+import android.os.Debug;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +23,10 @@ import com.opencharge.opencharge.domain.Entities.Point;
 import com.opencharge.opencharge.domain.use_cases.AddCommentUseCase;
 import com.opencharge.opencharge.presentation.locators.UseCasesLocator;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by Oriol on 10/4/2017.
@@ -35,32 +43,46 @@ public class PointsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
 
         private TextView adreca;
         private TextView access;
-        private TextView connector;
         private TextView lat;
         private TextView lng;
+        private LinearLayout connectorLayout;
+
+
 
         public ViewHolderPoint(View itemView) {
             super(itemView);
             adreca = (TextView) itemView.findViewById(R.id.adreca);
             access = (TextView) itemView.findViewById(R.id.access);
-            connector = (TextView) itemView.findViewById(R.id.connector);
+            connectorLayout = (LinearLayout) itemView.findViewById(R.id.connector_layout);
             lat = (TextView) itemView.findViewById(R.id.lat);
             lng = (TextView) itemView.findViewById(R.id.lng);
         }
 
         public final void bindPoint(Point p) {
             //Posar la informació d'un punt a la vista
+
             adreca.setText(p.getAddress());
             access.setText(p.getAccessType());
-            connector.setText(p.getConnectorType());
             lat.setText(String.valueOf(p.getLatCoord()));
             lng.setText(String.valueOf(p.getLonCoord()));
+
+
+            List<String> connectorList = p.getConnectorTypeList();
+            for(int i = 0; i < p.getConnectorTypeList().size(); ++i){
+                TextView connector = new TextView(itemView.getContext());
+                connector.setLayoutParams(new RecyclerView.LayoutParams(
+                        RecyclerView.LayoutParams.WRAP_CONTENT, RecyclerView.LayoutParams.WRAP_CONTENT));
+                connector.setText(connectorList.get(i));
+                connector.setPadding(0, 20, 0, 0);// in pixels (left, top, right, bottom)
+                connectorLayout.addView(connector);
+            }
 
             int drawable = Point.getDrawableForAccess(p.getAccessType());
             access.setCompoundDrawablesWithIntrinsicBounds(0, 0, drawable, 0);
 
-            drawable = Point.getDrawableForConnector(p.getConnectorType());
-            connector.setCompoundDrawablesWithIntrinsicBounds(0, 0, drawable, 0);
+            //drawable = Point.getDrawableForConnector(p.getConnectorType());
+            drawable = Point.getDrawableForConnector("TEST");
+            //connector.setCompoundDrawablesWithIntrinsicBounds(0, 0, drawable, 0);
         }
 
     }
