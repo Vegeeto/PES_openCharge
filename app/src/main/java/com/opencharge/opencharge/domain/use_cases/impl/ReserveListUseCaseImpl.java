f6e2a5dadfad0eb4685a728b5ad3bc12be3c7ea1
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-05-26 17:24:59 +0200

    added reservelist use case

---
+++ app/src/main/java/com/opencharge/opencharge/domain/use_cases/impl/ReserveListUseCaseImpl.java
@@ -0,0 +1,10 @@
+package com.opencharge.opencharge.domain.use_cases.impl;
+
+import com.opencharge.opencharge.domain.use_cases.ReservesListUseCase;
+
+/**
+ * Created by Crjs on 26/05/2017.
+ */
+
+public class ReserveListUseCaseImpl implements ReservesListUseCase {
+}
