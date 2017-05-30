7f316ad996cd24d526a8a613172328cfc0436f19
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-05-30 16:53:30 +0200

    function implementations

--- app/src/main/java/com/opencharge/opencharge/domain/repository/impl/FirebaseReserveRepository.java
+++ app/src/main/java/com/opencharge/opencharge/domain/repository/impl/FirebaseReserveRepository.java
@@ -2,17 +2,20 @@ package com.opencharge.opencharge.domain.repository.impl;
 
 import android.util.Log;
 
+import com.google.android.gms.games.snapshot.Snapshot;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.opencharge.opencharge.domain.Entities.FirebaseReserve;
+import com.opencharge.opencharge.domain.Entities.Point;
 import com.opencharge.opencharge.domain.Entities.Reserve;
 import com.opencharge.opencharge.domain.parsers.ReserveParser;
 import com.opencharge.opencharge.domain.parsers.impl.FirebaseReserveParser;
 import com.opencharge.opencharge.domain.repository.ReserveRepository;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 /**
@@ -23,6 +26,8 @@ public class FirebaseReserveRepository implements ReserveRepository {
 
     private ReserveParser reserveParser;
     private FirebaseDatabase database;
+    // We need a global var to count the childs to wait
+    private long childsToWait;
 
     //TODO: finish this class => ORIOL
 
@@ -71,6 +76,100 @@ public class FirebaseReserveRepository implements ReserveRepository {
 
     }
 
+    @Override
+    public void getReservesAsConsumerByUserId(String userId, final GetReservesByUserIdCallback callback) {
+        DatabaseReference myRef = database.getReference("Users");
+        myRef = myRef.child(userId);
+        myRef = myRef.child("ReservesConsumer");
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {;
+                final ArrayList<Reserve> reserves = new ArrayList<Reserve>();
+                childsToWait = dataSnapshot.getChildrenCount();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String reserveId = (String)snapshot.getValue();
+                    getReserveById(reserveId, new FirebaseReserveRepository.GetReserveByIdCallback(){
+                        @Override
+                        public void onReserveRetrieved(Reserve reserve) {
+                            reserves.add(reserve);
+                            if(--childsToWait < 1){
+                                callback.onReservesRetrieved(reserves);
+                            }
+                        }
+
+                        @Override
+                        public void onError() {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //TODO
+                Log.e("FirebaseRepo","ERROR: "+databaseError.toString());
+            }
+        });
+    }
+
+    @Override
+    public void getReservesAsSupplierByUserId(String userId, final GetReservesByUserIdCallback callback) {
+        DatabaseReference myRef = database.getReference("Users");
+        myRef = myRef.child(userId);
+        myRef = myRef.child("ReservesSupplier");
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {;
+                final ArrayList<Reserve> reserves = new ArrayList<Reserve>();
+                childsToWait = dataSnapshot.getChildrenCount();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    String reserveId = (String)snapshot.getValue();
+                    getReserveById(reserveId, new FirebaseReserveRepository.GetReserveByIdCallback(){
+                        @Override
+                        public void onReserveRetrieved(Reserve reserve) {
+                            reserves.add(reserve);
+                            if(--childsToWait < 1){
+                                callback.onReservesRetrieved(reserves);
+                            }
+                        }
+
+                        @Override
+                        public void onError() {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //TODO
+                Log.e("FirebaseRepo","ERROR: "+databaseError.toString());
+            }
+        });
+    }
+
+    @Override
+    public void getReserveById(String reserveId, final GetReserveByIdCallback callback) {
+        DatabaseReference myRef = database.getReference("Reserves");
+        myRef = myRef.child(reserveId);
+
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Reserve res = parseReserveFromSnapshot(dataSnapshot);
+                callback.onReserveRetrieved(res);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
     private Reserve[] parseReservesFromDataSnapshot(DataSnapshot dataSnapshot) {
         Reserve[] reserves = new Reserve[(int)dataSnapshot.getChildrenCount()];
         int index = 0;
@@ -84,7 +183,6 @@ public class FirebaseReserveRepository implements ReserveRepository {
 
         return reserves;
     }
-
     private Reserve parseReserveFromSnapshot(DataSnapshot snapshot) {
         if (snapshot.getValue() instanceof Map) {
             Map<String, Object> map = (Map<String, Object>) snapshot.getValue();
