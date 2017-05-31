be68f939e6641a4d7f87fbee7dbaf16052d2bef9
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-05-31 15:23:11 +0200

    push

--- app/src/main/java/com/opencharge/opencharge/domain/parsers/impl/FirebaseReserveParser.java
+++ app/src/main/java/com/opencharge/opencharge/domain/parsers/impl/FirebaseReserveParser.java
@@ -1,5 +1,6 @@
 package com.opencharge.opencharge.domain.parsers.impl;
 
+import com.opencharge.opencharge.domain.Entities.Point;
 import com.opencharge.opencharge.domain.Entities.Reserve;
 import com.opencharge.opencharge.domain.parsers.ReserveParser;
 import com.opencharge.opencharge.domain.parsers.ServiceParser;
@@ -15,6 +16,11 @@ public class FirebaseReserveParser extends AbstractParser implements ReservePars
     public static final String DAY_KEY = "day";
     public static final String START_HOUR_KEY = "startHour";
     public static final String END_HOUR_KEY = "endHour";
+    public static final String OWNER_FINISH = "ownerFinish";
+    public static final String SERVICE_ID = "servideId";
+    public static final String STATE = "state";
+    public static final String USER_FINISH = "userFinish";
+    public static final String USER_ID = "userId";
 
     //TODO: finish this class => ORIOL
 
@@ -25,7 +31,20 @@ public class FirebaseReserveParser extends AbstractParser implements ReservePars
         reserve.setDay(parseDateKeyFromMap(DAY_KEY, map));
         reserve.setStartHour(parseDateKeyFromMap(START_HOUR_KEY, map));
         reserve.setEndHour(parseDateKeyFromMap(END_HOUR_KEY, map));
-
+        reserve.setOwnerFinish(parseBooleanKeyFromMap(OWNER_FINISH, map));
+        reserve.setServiceId(parseStringKeyFromMap(SERVICE_ID, map));
+        reserve.setUserFinish(parseBooleanKeyFromMap(USER_FINISH, map));
+        reserve.setUserId(parseStringKeyFromMap(USER_ID, map));
+        String stateFirebase = parseStringKeyFromMap(STATE, map);
+        if (stateFirebase.equals("Creada")) {
+            reserve.setState(Reserve.CREATED);
+        }
+        if (stateFirebase.equals("Rebutjada")) {
+            reserve.setState(Reserve.REJECTED);
+        }
+        if (stateFirebase.equals("Acceptada")) {
+            reserve.setState(Reserve.ACCEPTED);
+        }
         return reserve;
     }
 
