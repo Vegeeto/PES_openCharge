22aa6742b55fd2b195284aaf76e4cced967f12e3
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-06-02 11:59:07 +0200

    new use case implementation

--- app/src/main/java/com/opencharge/opencharge/domain/use_cases/impl/ReservesUpdateConfirmationsUseCaseImpl.java
+++ app/src/main/java/com/opencharge/opencharge/domain/use_cases/impl/ReservesUpdateConfirmationsUseCaseImpl.java
@@ -4,21 +4,20 @@ import com.opencharge.opencharge.domain.Entities.Reserve;
 import com.opencharge.opencharge.domain.executor.Executor;
 import com.opencharge.opencharge.domain.executor.MainThread;
 import com.opencharge.opencharge.domain.repository.ReserveRepository;
-import com.opencharge.opencharge.domain.use_cases.ReservesUpdateUseCase;
-import com.opencharge.opencharge.domain.use_cases.ReservesUserAsConsumerUseCase;
+import com.opencharge.opencharge.domain.use_cases.ReservesUpdateConfirmationsUseCase;
 import com.opencharge.opencharge.domain.use_cases.base.AbstractUseCase;
 
 /**
  * Created by Crjs on 02/06/2017.
  */
 
-public class ReservesUpdateUseCaseImpl extends AbstractUseCase implements ReservesUpdateUseCase {
+public class ReservesUpdateConfirmationsUseCaseImpl extends AbstractUseCase implements ReservesUpdateConfirmationsUseCase {
     private ReserveRepository reserveRepository;
     private Reserve reserve;
 
-    public ReservesUpdateUseCaseImpl(Executor threadExecutor,
-                                             MainThread mainThread,
-                                             ReserveRepository reserveRepository) {
+    public ReservesUpdateConfirmationsUseCaseImpl(Executor threadExecutor,
+                                                  MainThread mainThread,
+                                                  ReserveRepository reserveRepository) {
         super(threadExecutor, mainThread);
 
         this.reserveRepository = reserveRepository;
