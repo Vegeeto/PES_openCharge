029fee7c17f9b874e7d8fd6d9ca9572f69734267
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-06-02 11:58:27 +0200

    splitted use case implementation

--- app/src/main/java/com/opencharge/opencharge/domain/use_cases/ReservesUserAsConsumerUseCase.java
+++ app/src/main/java/com/opencharge/opencharge/domain/use_cases/ReservesUserAsConsumerUseCase.java
@@ -1,8 +1,16 @@
 package com.opencharge.opencharge.domain.use_cases;
 
+import com.opencharge.opencharge.domain.Entities.Reserve;
+import com.opencharge.opencharge.domain.use_cases.base.UseCase;
+
 /**
  * Created by Crjs on 02/06/2017.
  */
 
 public interface ReservesUserAsConsumerUseCase {
+
+    interface Callback {
+        void onReservesRetrieved(Reserve[] reserves);
+    }
+    void setUserId(String userID);
 }
