f047dc6be84653a8f5fe5f1385e961b4f776074a
CRISTIAN-LAPTOP\Crjs <cristian.prat.93@gmail.com>
2017-06-06 18:56:05 +0200

    added "extends UseCase" to the Reserves Use Cases missing it

--- app/src/main/java/com/opencharge/opencharge/domain/use_cases/ReserveConfirmAsSupplierUseCase.java
+++ app/src/main/java/com/opencharge/opencharge/domain/use_cases/ReserveConfirmAsSupplierUseCase.java
@@ -1,11 +1,12 @@
 package com.opencharge.opencharge.domain.use_cases;
 
 import com.opencharge.opencharge.domain.Entities.Reserve;
+import com.opencharge.opencharge.domain.use_cases.base.UseCase;
 
 /**
  * Created by Crjs on 02/06/2017.
  */
 
-public interface ReserveConfirmAsSupplierUseCase {
+public interface ReserveConfirmAsSupplierUseCase extends UseCase {
     void setReserve(Reserve reserve);
 }
